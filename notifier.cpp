066662222ec91954d8a3885599c339f8d811e183
Christopher Peplin <cpeplin@ford.com>
2012-10-09 11:00:22 -0400

    Use basic tone() method to create some sound on pin 3.

--- notifier.cpp
+++ notifier.cpp
@@ -1,9 +1,14 @@
 #include "Arduino.h"
 
+const int SYNTH_PIN = 3;
+
 void setup() {
     Serial.begin(115200);
+    // pinMode(SYNTH_PIN, OUTPUT);
 }
 
 void loop() {
-    Serial.println("FOO");
+    for(int i = 0; i < 255; i++) {
+        tone(SYNTH_PIN, i * 50, 200);
+    }
 }
